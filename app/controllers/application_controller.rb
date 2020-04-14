83381c12889e4e7916ccfa2f4ce1ce96eda4cfbf
git-ryou2405 <r2405.exe@gmail.com>
2020-04-14 16:36:29 +0000

    Finish edit-attendances

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,33 @@ class ApplicationController < ActionController::Base
   
   $days_of_the_week = %w{日 月 火 水 木 金 土}
 
-# ページ出力前に1ヶ月分のデータの存在を確認・セットします。
+  # beforeフィルター
+
+  # paramsハッシュからユーザーを取得します。
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  # ログイン済みのユーザーか確認します。
+  def logged_in_user
+    store_location
+    unless logged_in?
+      flash[:danger] = "ログインしてください。"
+      redirect_to login_url
+    end
+  end
+
+  # アクセスしたユーザーが現在ログインしているユーザーか確認します。
+  def correct_user
+    redirect_to(root_url) unless current_user?(@user)
+  end
+  
+  # システム管理権限所有かどうか判定します。
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+
+  # ページ出力前に1ヶ月分のデータの存在を確認・セットします。
   def set_one_month
     @first_day = params[:date].nil? ?
     Date.current.beginning_of_month : params[:date].to_date
@@ -26,4 +52,5 @@ class ApplicationController < ActionController::Base
     flash[:danger] = "ページ情報の取得に失敗しました、再アクセスしてください。"
     redirect_to root_url
   end
+  
 end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,11 +3,20 @@ class Attendance < ApplicationRecord
   
   validates :worked_on, presence: true
   validates :note, length: { maximum: 50 }
-  
+
   # 出勤時間が存在しない場合、退勤時間は無効
   validate :finished_at_is_invalid_without_a_started_at
+  # 出勤・退勤時間どちらも存在する時、出勤時間より早い退勤時間は無効
+  validate :started_at_than_finished_at_fast_if_invalid
   
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
   end
+  
+  def started_at_than_finished_at_fast_if_invalid
+    if started_at.present? && finished_at.present?
+      errors.add(:started_at, "より早い退勤時間は無効です") if started_at > finished_at
+    end
+  end
+  
 end
